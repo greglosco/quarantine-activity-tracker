256d76f1481151c16a60bbafe754af16b8555d9b
Greg Losco <gtlosco@gmail.com>
2020-05-07 19:21:51 -0700

    added devtools extension to connect thunk and devtools to store

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from './App';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/manageActivities'
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk)
+))
 
 ReactDOM.render(
   <Provider store={store}>
