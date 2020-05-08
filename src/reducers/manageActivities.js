d06cfcd5e2b41b536f93d0d00f2b07ee1860f51b
Greg Losco <gtlosco@gmail.com>
2020-05-07 19:16:35 -0700

    started coding fitness activities reducer

--- src/reducers/manageActivities.js
+++ src/reducers/manageActivities.js
@@ -1,2 +1,20 @@
 import { combineReducers } from 'redux';
 import cuid from 'cuid';
+
+const rootReducer = combineReducers({
+    fitnessActivities: fitnessActivitiesReducer,
+    recipes: recipesReducer,
+    hobbies: hobbiesReducer,
+    livestreams: livestreamsReducer
+});
+
+export default rootReducer;
+
+function fitnessActivitiesReducer(state = {
+    fitnessActivities: [],
+}, action) {
+    switch (action.type) {
+        case 'ADD_FITNESS_ACTIVITY':
+            return {...state, fitnessActivities: [...state.fitnessActivities, action.exercise]}
+    }
+}
