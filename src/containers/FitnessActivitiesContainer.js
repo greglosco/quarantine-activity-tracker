705840ce9a2998e918a86674bc7ca13ad7087e48
Greg Losco <gtlosco@gmail.com>
2020-05-08 11:59:36 -0700

    added fitness activity and fitness activities components, imported the latter to fitness activity container

--- src/containers/FitnessActivitiesContainer.js
+++ src/containers/FitnessActivitiesContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FitnessActivitiesInput from '../components/fitnessactivities/FitnessActivitiesInput';
+import FitnessActivities from '../components/fitnessactivities/FitnessActivities';
 import { connect } from 'react-redux'
 
 class FitnessActivitiesContainer extends Component {
@@ -8,6 +9,7 @@ class FitnessActivitiesContainer extends Component {
         return (
             <div>FitnessActivitiesContainer
                 <FitnessActivitiesInput addFitnessActivity={this.props.addFitnessActivity} />
+                <FitnessActivities fitnessactivities={this.props.fitnessactivities} />
             </div>
         )
     }
