e8895885285c262ddf3bf5fd78084ef1ba78c0cf
Greg Losco <gtlosco@gmail.com>
2020-05-08 09:45:54 -0700

    imported router, wrapped app in router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route 
+} from 'react-router-dom'
 import FitnessActivitiesContainer from './containers/FitnessActivitiesContainer';
 import RecipesContainer from './containers/RecipesContainer';
 import HobbiesContainer from './containers/HobbiesContainer';
@@ -8,20 +12,22 @@ import LivestreamsContainer from './containers/LivestreamsContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the Quarantine Activity Tracker!
-        </p>
-      </header>
-      <main>
-        <FitnessActivitiesContainer />
-        <RecipesContainer />
-        <HobbiesContainer />
-        <LivestreamsContainer />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to the Quarantine Activity Tracker!
+          </p>
+        </header>
+        <main>
+          <FitnessActivitiesContainer />
+          <RecipesContainer />
+          <HobbiesContainer />
+          <LivestreamsContainer />
+        </main>
+      </div>
+    </Router>
   );
 }
 
