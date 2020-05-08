ad85e1096579659b6377c9e1db4d807f83d5df17
Greg Losco <gtlosco@gmail.com>
2020-05-07 19:54:43 -0700

    reducers stubbed out

--- src/reducers/manageActivities.js
+++ src/reducers/manageActivities.js
@@ -18,3 +18,30 @@ function fitnessActivitiesReducer(state = {
             return {...state, fitnessActivities: [...state.fitnessActivities, action.exercise]}
     }
 }
+
+function recipesReducer(state = {
+    recipes: [],
+}, action) {
+    switch (action.type) {
+        case 'ADD_RECIPE':
+            return {...state, recipes: [...state.recipes, action.name]}
+    }
+}
+
+function hobbiesReducer(state = {
+    hobbies: [],
+}, action) {
+    switch (action.type) {
+        case 'ADD_HOBBY':
+            return {...state, hobbies: [...state.hobbies, action.exercise]}
+    }
+}
+
+function livestreamsReducer(state = {
+    livestreams: [],
+}, action) {
+    switch (action.type) {
+        case 'ADD_LIVESTREAM':
+            return {...state, livestreams: [...state.livestreams, action.exercise]}
+    }
+}
