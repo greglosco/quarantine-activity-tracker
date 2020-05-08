ac43b6ddfa45fc9b00e2821839b515729d4aaab0
Greg Losco <gtlosco@gmail.com>
2020-05-08 10:00:10 -0700

    stubbed out initial fitness activities container

---
+++ src/containers/FitnessActivitiesContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class FitnessActivitiesContainer extends Component {
+
+    render() {
+        return (
+            <div>FitnessActivitiesContainer</div>
+        )
+    }
+}
+
+export default FitnessActivitiesContainer;
