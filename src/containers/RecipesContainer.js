d66b87477161e766c212ba346c250ae8cd5d2c53
Greg Losco <gtlosco@gmail.com>
2020-05-08 10:01:42 -0700

    stubbed out initial recipes container

---
+++ src/containers/RecipesContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class RecipesContainer extends Component {
+
+    render() {
+        return (
+            <div>RecipesContainer</div>
+        )
+    }
+}
+
+export default RecipesContainer;
