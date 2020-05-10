8405027e98ab85295cb03e3486a3cb29f257adf5
Greg Losco <gtlosco@gmail.com>
2020-05-09 19:44:33 -0700

    fitness activities controller actions stubbed out

--- rails-backend/app/controllers/fitness_activities_controller.rb
+++ rails-backend/app/controllers/fitness_activities_controller.rb
@@ -1,2 +1,16 @@
 class FitnessActivitiesController < ApplicationController
+
+    def index
+    end
+
+    def create
+        FitnessActivity.create(fitness_activity_params)
+    end
+
+    private
+
+    def fitness_activity_params
+        params.require(:fitnessActivity).permit(:exercise, :date, :notes)
+    end
+
 end
