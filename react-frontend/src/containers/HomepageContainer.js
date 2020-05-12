9d4ea130ac56f7d46cb5d1ae623712f987320f28
Greg Losco <gtlosco@gmail.com>
2020-05-11 17:18:43 -0700

    rendered fitness activities component in homepage container

--- react-frontend/src/containers/HomepageContainer.js
+++ react-frontend/src/containers/HomepageContainer.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import FitnessActivities from '../components/fitnessactivities/FitnessActivities';
+import { fetchFitnessActivities } from '../actions/fitnessActivities';
+import { connect } from 'react-redux';
+
+
 
 class HomepageContainer extends Component {
 
+    componentDidMount() {
+        this.props.fetchFitnessActivities()
+    }
+
     render() {
         return (
-            <div>HomepageContainer</div>
+            <div>HomepageContainer
+                <FitnessActivities fitnessActivities={this.props.fitnessActivities} />
+            </div>
         )
     }
 }
 
-export default HomepageContainer;
+const mapStateToProps = ({fitnessActivities}) => ({fitnessActivities})
+
+export default connect(mapStateToProps, { fetchFitnessActivities })(HomepageContainer);
