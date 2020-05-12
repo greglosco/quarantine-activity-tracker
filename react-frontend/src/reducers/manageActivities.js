d01e38f81b676b81d74ed3f4d5b85f998f3c5fa4
Greg Losco <gtlosco@gmail.com>
2020-05-11 17:25:55 -0700

    added delete fa action into reducer

--- react-frontend/src/reducers/manageActivities.js
+++ react-frontend/src/reducers/manageActivities.js
@@ -3,9 +3,9 @@ import cuid from 'cuid';
 
 const rootReducer = combineReducers({
     fitnessActivities: fitnessActivitiesReducer,
-    recipes: recipesReducer,
-    hobbies: hobbiesReducer,
-    livestreams: livestreamsReducer
+    // recipes: recipesReducer,
+    // hobbies: hobbiesReducer,
+    // livestreams: livestreamsReducer
 });
 
 export default rootReducer;
@@ -17,34 +17,37 @@ function fitnessActivitiesReducer(state = [], action) {
             return [...state, fitnessActivity]
         case 'RENDER_FITNESS_ACTIVITIES':
             return [...state, ...action.res]
+        case 'DELETE_FITNESS_ACTIVITY':
+            const fitnessActivities = state.filter(obj => obj.id !== action.id)
+            return {...state, fitnessActivities}
         default:
             return state;
     }
 }
 
-function recipesReducer(state = [], action) {
-    switch (action.type) {
-        case 'ADD_RECIPE':
-            return {...state, recipes: [...state.recipes, action.name]}
-        default:
-            return state;
-    }
-}
+// function recipesReducer(state = [], action) {
+//     switch (action.type) {
+//         case 'ADD_RECIPE':
+//             return {...state, recipes: [...state.recipes, action.name]}
+//         default:
+//             return state;
+//     }
+// }
 
-function hobbiesReducer(state = [], action) {
-    switch (action.type) {
-        case 'ADD_HOBBY':
-            return {...state, hobbies: [...state.hobbies, action.exercise]}
-        default:
-            return state;
-    }
-}
+// function hobbiesReducer(state = [], action) {
+//     switch (action.type) {
+//         case 'ADD_HOBBY':
+//             return {...state, hobbies: [...state.hobbies, action.exercise]}
+//         default:
+//             return state;
+//     }
+// }
 
-function livestreamsReducer(state = [], action) {
-    switch (action.type) {
-        case 'ADD_LIVESTREAM':
-            return {...state, livestreams: [...state.livestreams, action.exercise]}
-        default:
-            return state;
-    }
-}
+// function livestreamsReducer(state = [], action) {
+//     switch (action.type) {
+//         case 'ADD_LIVESTREAM':
+//             return {...state, livestreams: [...state.livestreams, action.exercise]}
+//         default:
+//             return state;
+//     }
+// }
