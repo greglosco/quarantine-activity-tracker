eaad3ac513335375a8a5395e59e7c30872b9dc13
Greg Losco <gtlosco@gmail.com>
2020-05-12 17:50:11 -0700

    livestreams container rendered in homepage container

--- react-frontend/src/containers/HomepageContainer.js
+++ react-frontend/src/containers/HomepageContainer.js
@@ -8,6 +8,9 @@ import { fetchRecipes, deleteRecipe } from '../actions/recipes';
 import Hobbies from '../components/hobbies/Hobbies';
 import { fetchHobbies, deleteHobby } from '../actions/hobbies';
 
+import Livestreams from '../components/livestreams/Livestreams';
+import { fetchLivestreams, deleteLivestream } from '../actions/livestreams';
+
 import { connect } from 'react-redux';
 
 
@@ -20,11 +23,12 @@ class HomepageContainer extends Component {
                 Your Fitness Activities:<FitnessActivities fitnessActivities={this.props.fitnessActivities} fetchFitnessActivities={this.props.fetchFitnessActivities} deleteFitnessActivity={this.props.deleteFitnessActivity} />
                 Your Recipes:<Recipes recipes={this.props.recipes} fetchRecipes={this.props.fetchRecipes} deleteRecipe={this.props.deleteRecipe} />
                 Your Hobbies:<Hobbies hobbies={this.props.hobbies} fetchHobbies={this.props.fetchHobbies} deleteHobby={this.props.deleteHobby} />
+                Your Livestreams:<Livestreams livestreams={this.props.livestreams} fetchLivestreams={this.props.fetchLivestreams} deleteLivestream={this.props.deleteLivestream} />
             </div>
         )
     }
 }
 
-const mapStateToProps = ({fitnessActivities, recipes, hobbies}) => ({fitnessActivities, recipes, hobbies})
+const mapStateToProps = ({fitnessActivities, recipes, hobbies, livestreams}) => ({fitnessActivities, recipes, hobbies, livestreams})
 
-export default connect(mapStateToProps, { fetchFitnessActivities, deleteFitnessActivity, fetchRecipes, deleteRecipe, fetchHobbies, deleteHobby })(HomepageContainer);
+export default connect(mapStateToProps, { fetchFitnessActivities, deleteFitnessActivity, fetchRecipes, deleteRecipe, fetchHobbies, deleteHobby, fetchLivestreams, deleteLivestream })(HomepageContainer);
