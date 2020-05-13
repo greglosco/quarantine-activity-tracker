9b3e3c6dca12bbe47e583389bf368f1b22158826
Greg Losco <gtlosco@gmail.com>
2020-05-12 17:43:41 -0700

    livestreams.js component coded

---
+++ react-frontend/src/components/livestreams/Livestreams.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Livestream from './Livestream';
+import EditLivestream from './EditLivestream'
+
+class Livestreams extends Component {
+
+    componentDidMount() {
+       this.props.fetchLivestreams()
+    }
+
+    render() {
+
+        const livestreams = this.props.livestreams.map(livestream => (livestream.editing ? <EditLivestream key={livestream.id} livestream={livestream} updateLivestream={this.props.updateLivestream} /> : <Livestream key={livestream.id} livestream={livestream} deleteLivestream={this.props.deleteLivestream} editLivestream={this.props.editLivestream} />))
+
+        return (
+            <ul>
+                {livestreams}
+            </ul>
+        )
+    }
+}
+
+export default Livestreams;
