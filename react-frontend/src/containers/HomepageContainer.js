5913b5deb8819a68b957460e3e2d464123b4df7f
Greg Losco <gtlosco@gmail.com>
2020-05-12 17:26:52 -0700

    hobbies component rendered in homepage container

--- react-frontend/src/containers/HomepageContainer.js
+++ react-frontend/src/containers/HomepageContainer.js
@@ -5,6 +5,9 @@ import { fetchFitnessActivities, deleteFitnessActivity } from '../actions/fitnes
 import Recipes from '../components/recipes/Recipes';
 import { fetchRecipes, deleteRecipe } from '../actions/recipes';
 
+import Hobbies from '../components/hobbies/Hobbies';
+import { fetchHobbies, deleteHobby } from '../actions/hobbies';
+
 import { connect } from 'react-redux';
 
 
@@ -16,11 +19,12 @@ class HomepageContainer extends Component {
             <div>
                 Your Fitness Activities:<FitnessActivities fitnessActivities={this.props.fitnessActivities} fetchFitnessActivities={this.props.fetchFitnessActivities} deleteFitnessActivity={this.props.deleteFitnessActivity} />
                 Your Recipes:<Recipes recipes={this.props.recipes} fetchRecipes={this.props.fetchRecipes} deleteRecipe={this.props.deleteRecipe} />
+                Your Hobbies:<Hobbies hobbies={this.props.hobbies} fetchHobbies={this.props.fetchHobbies} deleteHobby={this.props.deleteHobby} />
             </div>
         )
     }
 }
 
-const mapStateToProps = ({fitnessActivities, recipes}) => ({fitnessActivities, recipes})
+const mapStateToProps = ({fitnessActivities, recipes, hobbies}) => ({fitnessActivities, recipes, hobbies})
 
-export default connect(mapStateToProps, { fetchFitnessActivities, deleteFitnessActivity, fetchRecipes, deleteRecipe })(HomepageContainer);
+export default connect(mapStateToProps, { fetchFitnessActivities, deleteFitnessActivity, fetchRecipes, deleteRecipe, fetchHobbies, deleteHobby })(HomepageContainer);
