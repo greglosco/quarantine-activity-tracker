cf2cf9d00532a828be8fc7d4a0bd42b509cd16bc
Greg Losco <gtlosco@gmail.com>
2020-05-12 17:02:44 -0700

    rendered recipes component in homepage container

--- react-frontend/src/containers/HomepageContainer.js
+++ react-frontend/src/containers/HomepageContainer.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import FitnessActivities from '../components/fitnessactivities/FitnessActivities';
 import { fetchFitnessActivities, deleteFitnessActivity } from '../actions/fitnessActivities';
+
+import Recipes from '../components/recipes/Recipes';
+import { fetchRecipes, deleteRecipe } from '../actions/recipes';
+
 import { connect } from 'react-redux';
 
 
@@ -11,11 +15,12 @@ class HomepageContainer extends Component {
         return (
             <div>
                 Your Fitness Activities:<FitnessActivities fitnessActivities={this.props.fitnessActivities} fetchFitnessActivities={this.props.fetchFitnessActivities} deleteFitnessActivity={this.props.deleteFitnessActivity} />
+                Your Recipes:<Recipes recipes={this.props.recipes} fetchRecipes={this.props.fetchRecipes} deleteRecipe={this.props.deleteRecipe} />
             </div>
         )
     }
 }
 
-const mapStateToProps = ({fitnessActivities}) => ({fitnessActivities})
+const mapStateToProps = ({fitnessActivities, recipes}) => ({fitnessActivities, recipes})
 
-export default connect(mapStateToProps, { fetchFitnessActivities, deleteFitnessActivity })(HomepageContainer);
+export default connect(mapStateToProps, { fetchFitnessActivities, deleteFitnessActivity, fetchRecipes, deleteRecipe })(HomepageContainer);
