c2b46a8172ec1911ae2e819c016be8050ed87a2c
Greg Losco <gtlosco@gmail.com>
2020-05-14 12:42:53 -0700

    formatted header

--- react-frontend/src/components/NavBar.js
+++ react-frontend/src/components/NavBar.js
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div className="navbar">
-            <NavLink className="nav-link" activeClassName="active" exact to="/">Homepage</NavLink>
-            <NavLink className="nav-link" activeClassName="active" to="/fitnessactivities">Fitness Activities</NavLink>
-            <NavLink className="nav-link" activeClassName="active" to="/recipes">Recipes</NavLink>
-            <NavLink className="nav-link" activeClassName="active" to="/hobbies">Hobbies</NavLink>
-            <NavLink className="nav-link" activeClassName="active" to="/livestreams">Livestreams</NavLink>
+        <div className="ui inverted fluid five item menu">
+                <NavLink className="item" activeClassName="active" exact to="/">Homepage</NavLink>
+                <NavLink className="item" activeClassName="active" to="/fitnessactivities">Fitness Activities</NavLink>
+                <NavLink className="item" activeClassName="active" to="/recipes">Recipes</NavLink>
+                <NavLink className="item" activeClassName="active" to="/hobbies">Hobbies</NavLink>
+                <NavLink className="item" activeClassName="active" to="/livestreams">Livestreams</NavLink>
         </div> 
     )
 }
