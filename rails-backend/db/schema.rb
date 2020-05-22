7139f9ed8916ef5b14f5522afdee0cfa324bbae6
Greg Losco <gtlosco@gmail.com>
2020-05-22 08:48:08 -0700

    postgres

--- rails-backend/db/schema.rb
+++ rails-backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_13_003136) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "fitness_activities", force: :cascade do |t|
     t.string "exercise"
     t.date "date"
