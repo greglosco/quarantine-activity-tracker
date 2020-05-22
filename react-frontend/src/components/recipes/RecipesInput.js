893272d579e4c22ebb28d754cf433e19d28e1a6c
Greg Losco <gtlosco@gmail.com>
2020-05-22 11:31:24 -0700

    action added to recipes form

--- react-frontend/src/components/recipes/RecipesInput.js
+++ react-frontend/src/components/recipes/RecipesInput.js
@@ -32,7 +32,7 @@ class RecipesInput extends Component {
             <div className="ui fluid green card" >
                 <div className="content">
                     <div className="header">Track Your Recipes:</div><br/>
-                    <form className="ui form" onSubmit={this.handleOnSubmit} name="form" data-netlify="true" netlify-honeypot="bot-field">
+                    <form className="ui form" onSubmit={this.handleOnSubmit} name="form" data-netlify="true" netlify-honeypot="bot-field" action="/recipes">
                     <input type="hidden" name="form-name" value="form" />
                         <div className="field">
                             <label>Recipe Name: </label><input type="text" value={this.state.name} name="name" onChange={this.handleOnChange} /><br/>
