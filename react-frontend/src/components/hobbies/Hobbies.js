0fd8a2d9ca1e971d5f55667a1a46fe37ad18d5ba
Greg Losco <gtlosco@gmail.com>
2020-05-22 15:57:56 -0700

    added sort hobbies button

--- react-frontend/src/components/hobbies/Hobbies.js
+++ react-frontend/src/components/hobbies/Hobbies.js
@@ -8,16 +8,37 @@ class Hobbies extends Component {
        this.props.fetchHobbies()
     }
 
+    state = {
+        hobbies: []
+    }
+
+    handleOnClick = () => {
+        const sortedHobbies = [...this.props.hobbies].sort((a, b) => {
+            if (b.name > a.name) {
+                return -1
+            } else if (a.name < b.name) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+        this.setState({
+            hobbies: [...sortedHobbies]
+        })
+    }
+
     render() {
 
-        const hobbies = this.props.hobbies.map(hobby => (hobby.editing ? <EditHobby key={hobby.id} hobby={hobby} updateHobby={this.props.updateHobby} /> : <Hobby key={hobby.id} hobby={hobby} deleteHobby={this.props.deleteHobby} editHobby={this.props.editHobby} />))
+        const hobbies = this.state.hobbies.length === 0 ? this.props.hobbies : this.state.hobbies
+        const renderedHobbies = hobbies.map(hobby => (hobby.editing ? <EditHobby key={hobby.id} hobby={hobby} updateHobby={this.props.updateHobby} /> : <Hobby key={hobby.id} hobby={hobby} deleteHobby={this.props.deleteHobby} editHobby={this.props.editHobby} />))
 
         return (
             <div>
                 <ul class="ui red fluid card">
                     <div className="content">
                         <div className="header">Your Hobbies:<br/></div>
-                        {hobbies}
+                        <button class="ui button" onClick={this.handleOnClick} >Sort Alphabetically by Name</button>
+                        {renderedHobbies}
                     </div>
                 </ul>
             </div>
