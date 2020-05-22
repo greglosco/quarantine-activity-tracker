944697c65a68c8441c2d9578095b563e44cea8f3
Greg Losco <gtlosco@gmail.com>
2020-05-22 16:02:00 -0700

    added sort recipes button

--- react-frontend/src/components/recipes/Recipes.js
+++ react-frontend/src/components/recipes/Recipes.js
@@ -8,16 +8,38 @@ class Recipes extends Component {
        this.props.fetchRecipes()
     }
 
+    state = {
+        recipes: []
+    }
+
+    handleOnClick = () => {
+        const sortedRecipes = [...this.props.recipes].sort((a, b) => {
+            if (b.name > a.name) {
+                return -1
+            } else if (a.name < b.name) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+        this.setState({
+            recipes: [...sortedRecipes]
+        })
+    }
+
+
     render() {
 
-        const recipes = this.props.recipes.map(recipe => (recipe.editing ? <EditRecipe key={recipe.id} recipe={recipe} updateRecipe={this.props.updateRecipe} /> : <Recipe key={recipe.id} recipe={recipe} deleteRecipe={this.props.deleteRecipe} editRecipe={this.props.editRecipe} />))
-
+        const recipes = this.state.recipes.length === 0 ? this.props.recipes : this.state.recipes
+        const renderedRecipes = recipes.map(recipe => (recipe.editing ? <EditRecipe key={recipe.id} recipe={recipe} updateRecipe={this.props.updateRecipe} /> : <Recipe key={recipe.id} recipe={recipe} deleteRecipe={this.props.deleteRecipe} editRecipe={this.props.editRecipe} />))
+        
         return (
             <div>
                 <ul className="ui fluid purple card">
                     <div className="content">
                         <div className="header">Your Recipes:<br/></div>
-                        {recipes}
+                        <button class="ui button" onClick={this.handleOnClick} >Sort Alphabetically by Name</button>
+                        {renderedRecipes}
                     </div>
                 </ul>
             </div>
